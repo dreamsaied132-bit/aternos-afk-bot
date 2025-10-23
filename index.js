5e891398e1eab08f1e51305a6ac413e2d83df44d
Abhaymurashilli <abhaymurashilli@gmail.com>
2025-10-23 15:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'YOUR_SERVER_IP', // without port (ex: play.aternos.me)
-  port: 25565, // Default Minecraft port
-  username: 'AFK_Bot' // Bot username
+  host: 'CrystalCraftPLAY.aternos.me', // without port (ex: play.aternos.me)
+  port: 12865, // Default Minecraft port
+  username: 'AFK_24/7' // Bot username
 });
 
 bot.on('spawn', () => {
